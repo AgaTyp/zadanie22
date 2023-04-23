37534ba0ea9fe54f7c3faf86636bc6a4ac77dc8e
Agnieszka Typiak <agnieszka.typiak@syncron.com>
2023-04-23 21:20:39 +0200

    stworzenie_struktury

---
+++ src/test/java/pl/agntyp/zadanie22/Zadanie22ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.agntyp.zadanie22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zadanie22ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
